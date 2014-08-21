f4bae605fb8b86fe74bb4eee175cb9c7530a636b
kevish <albinokevrein@yahoo.com>
2014-08-20 22:07:22 -0600

    server error
    server error

--- assets/js/app.js
+++ assets/js/app.js
@@ -126,7 +126,7 @@ var activityareas = L.geoJson(null, {
                 activityareas.resetStyle(e.target);
             },
             click: function (e) {
-                window.open('file:///C:/projects/bootleaf-master/bootleaf-master/Completed_Surveys-2014-06-16/Completed%20Surveys/' + feature.properties.name + '.pdf');
+                window.open('surv/' + feature.properties.name + '.pdf');
             }
         }).bindLabel(feature.properties.name);
         /*boroughSearch.push({
@@ -165,7 +165,7 @@ var boroughs = L.geoJson(null, {
                 boroughs.resetStyle(e.target);
             },
             click: function (e) {
-                window.open('file:///C:/projects/bootleaf-master/bootleaf-master/Completed_Surveys-2014-06-16/Completed%20Surveys/' + feature.properties.NAME + '.html');
+                window.open('surv/' + feature.properties.NAME + '.html');
             }
         }).bindLabel(feature.properties.NAME);
         /*onEachFeature: function (feature, layer) {
